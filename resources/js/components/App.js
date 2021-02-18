ab2baabfd012f9d3577515d28cff9d4183bf3d13
Furkan <furkvnclk@gmail.com>
2021-02-18 13:20:40 +0300

    Update App.js

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import ProjectsList from './ProjectsList'
+import CreateProject from "./CreateProject";
 
 class App extends Component {
     render() {
@@ -9,8 +11,14 @@ class App extends Component {
             <BrowserRouter>
                 <div>
                     <Header />
+                    <Switch>
+                        <Route exact path='/' component={ProjectsList} />
+                        <Route exact path='/create' component={CreateProject} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
     }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'))
