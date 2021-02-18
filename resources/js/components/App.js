8d5fde533d3a3af68908d45bd4f9489204a95bdb
Furkan <furkvnclk@gmail.com>
2021-02-18 12:22:32 +0300

    Create App.js
    The main react page of the system where everything will be loaded

---
+++ resources/js/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
