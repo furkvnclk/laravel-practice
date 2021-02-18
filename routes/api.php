25bc7c35e839becbd2684a0ce54baabdca9ed3bf
Furkan <furkvnclk@gmail.com>
2021-02-18 12:21:49 +0300

    Creating the app API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('projects', [ProjectController::class, 'index']);
+Route::post('projects', [ProjectController::class, 'store']);
+Route::get('projects/{id}', [ProjectController::class, 'show']);
+Route::put('projects', [ProjectController::class, 'markAsCompleted']);
+Route::get('tasks', [TaskController::class, 'store']);
+Route::get('tasks/{task}', [TaskController::class, 'markAsCompleted']);
