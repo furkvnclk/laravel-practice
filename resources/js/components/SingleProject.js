454f64de446cbc6f735dc48c60c2b229288815d7
Furkan <furkvnclk@gmail.com>
2021-02-19 10:50:57 +0300

    Update SingleProject.js

--- resources/js/components/SingleProject.js
+++ resources/js/components/SingleProject.js
@@ -7,8 +7,17 @@ class SingleProject extends Component {
     super(props)
     this.state = {
       project: {},
-      tasks: []
+      tasks: [],
+      title: '',
+      errors: []
     }
+
+    this.handleMarkProjectAsCompleted = this.handleMarkProjectAsCompleted.bind(this)
+    this.handleMarkTaskAsCompleted = this.handleMarkTaskAsCompleted.bind(this)
+    this.handleFieldChange = this.handleFieldChange.bind(this)
+    this.handleAddNewTask = this.handleAddNewTask.bind(this)
+    this.hasErrorFor = this.hasErrorFor.bind(this)
+    this.renderErrorFor = this.renderErrorFor.bind(this)
   }
 
   componentDidMount() {
@@ -23,6 +32,68 @@ class SingleProject extends Component {
       })
   }
 
+  handleMarkProjectAsCompleted() {
+    const { history } = this.props
+
+    axios.put(`/api/projects/${this.state.project.id}`)
+      .then(response => history.push('/'))
+  }
+
+  handleMarkTaskAsCompleted(taskId) {
+    axios.put(`/api/tasks/${taskId}`).then(response => {
+      this.setState(prevState => ({
+        tasks: prevState.tasks.filter(task => {
+          return task.id !== taskId
+        })
+      }))
+    })
+  }
+
+  handleFieldChange(event) {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  handleAddNewTask(event) {
+    event.preventDefault()
+
+    const task = {
+      title: this.state.title,
+      project_id: this.state.project.id
+    }
+
+    axios.post('/api/tasks', task)
+      .then(response => {
+        // clear form input
+        this.setState({
+          title: ''
+        })
+        // add new task to list of tasks
+        this.setState(prevState => ({
+          tasks: prevState.tasks.concat(response.data)
+        }))
+      }).catch(error => {
+        this.setState({
+          errors: error.response.data.errors
+        })
+      })
+  }
+
+  hasErrorFor(field) {
+    return !!this.state.errors[field]
+  }
+
+  renderErrorFor(field) {
+    if (this.hasErrorFor(field)) {
+      return (
+        <span className='invalid-feedback'>
+          <strong>{this.state.errors[field][0]}</strong>
+        </span>
+      )
+    }
+  }
+
   render() {
     const { project, tasks } = this.state
 
@@ -35,11 +106,31 @@ class SingleProject extends Component {
               <div className='card-body'>
                 <p>{project.description}</p>
 
-                <button className='btn btn-primary btn-sm'>
+                <button
+                  className='btn btn-primary btn-sm'
+                  onClick={this.handleMarkProjectAsCompleted}
+                >
                   Mark As Completed
                 </button>
 
                 <hr />
+
+                <form onSubmit={this.handleAddNewTask}>
+                  <div className='input-group'>
+                    <input
+                      type='text'
+                      name='title'
+                      className={`form-control ${this.hasErrorFor('title') ? 'is-invalid' : ''}`}
+                      placeholder='Task title'
+                      value={this.state.title}
+                      onChange={this.handleFieldChange}
+                    />
+                    <div className='input-group-append'>
+                      <button className='btn btn-primary'>Add</button>
+                    </div>
+                    {this.renderErrorFor('title')}
+                  </div>
+                </form>
 
                 <ul className='list-group mt-3'>
                   {tasks.map(task => (
@@ -49,7 +140,10 @@ class SingleProject extends Component {
                     >
                       {task.title}
 
-                      <button className='btn btn-primary btn-sm'>
+                      <button
+                        className='btn btn-primary btn-sm'
+                        onClick={this.handleMarkTaskAsCompleted.bind(this, task.id)}
+                      >
                         Mark as completed
                       </button>
                     </li>
